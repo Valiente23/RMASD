f15fbac0652d4cb48004b18018049d0209233904
Braian Oliva <Braianoliva23@gmail,com>
2022-01-27 22:20:47 -0300

    Mobile 55%

--- js/main.js
+++ js/main.js
@@ -16,5 +16,36 @@ function removerNav() {
     });
   }
 }
+function carrusel() {
+  const slider = document.querySelector(".container-clientes");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener("mouseleave", (_) => {
+    isDown = false;
+    slider.classList.remove("active");
+  });
+  slider.addEventListener("mouseup", (_) => {
+    isDown = false;
+    slider.classList.remove("active");
+  });
+  slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const SCROLL_SPEED = 1;
+    const walk = (x - startX) * SCROLL_SPEED;
+    slider.scrollLeft = scrollLeft - walk;
+  });
+}
+
 deslizarNav();
 removerNav();
+carrusel();
